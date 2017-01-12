130dd6a5d10cd3f8e6603de3f86f3402b3e4a75a
Akanksha <akanksharani.hegde@robosoftin.com>
2017-01-12 18:58:00 +0530

    Included splash screen

---
+++ app/src/main/java/com/gmat/terminator/activity/SplashActivity.java
@@ -0,0 +1,52 @@
+package com.gmat.terminator.activity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+import com.gmat.terminator.R;
+
+/**
+ * Created by Akanksha on 12-Jan-17.
+ */
+
+public class SplashActivity extends Activity {
+
+    // Splash screen timer
+    private static int SPLASH_TIME_OUT = 3000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        ImageView image = (ImageView)findViewById(R.id.imgLogo);
+
+        final Animation animationFadeIn = AnimationUtils.loadAnimation(this, R.anim.fade_in);
+        image.startAnimation(animationFadeIn);
+
+        new Handler().postDelayed(new Runnable() {
+
+            /*
+             * Showing splash screen with a timer. This will be useful when you
+             * want to show case your app logo / company
+             */
+
+            @Override
+            public void run() {
+                // This method will be executed once the timer is over
+                // Start your app main activity
+                Intent i = new Intent(SplashActivity.this, RegistrationActivity.class);
+                startActivity(i);
+
+                // close this activity
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
+    }
+
+}
