68ff5d735206874182acc3b4ecbc480177534bf4
Akanksha Hegde <hegdeakanksha@gmail.com>
2017-01-12 22:50:58 +0530

    Included Template files

---
+++ app/src/main/java/com/gmat/terminator/activity/AddSectionInfoActivity.java
@@ -0,0 +1,10 @@
+package com.gmat.terminator.activity;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Akanksha Hegde on 12-01-2017.
+ */
+
+public class AddSectionInfoActivity extends AppCompatActivity {
+}
--- app/src/main/java/com/gmat/terminator/fragment/TemplatesFragment.java
+++ app/src/main/java/com/gmat/terminator/fragment/TemplatesFragment.java
@@ -1,48 +1,43 @@
 package com.gmat.terminator.fragment;
 
+import android.app.Dialog;
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.gmat.terminator.R;
 
-public class TemplatesFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public TemplatesFragment() {
+public class TemplatesFragment extends Fragment implements View.OnClickListener {
+     private TextView mAddTemplateBtn;
+       public TemplatesFragment() {
         // Required empty public constructor
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
         }
-    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_create_test, null);
+        View view = inflater.inflate(R.layout.fragment_templates, null);
 
         initializeViews(view);
         return view;
     }
 
     private void initializeViews(View view) {
-
+        mAddTemplateBtn = (TextView) view.findViewById(R.id.add_template_btn);
+        mAddTemplateBtn.setOnClickListener(this);
     }
 
     @Override
@@ -56,4 +51,31 @@ public class TemplatesFragment extends Fragment {
     }
 
 
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.add_template_btn:
+                showAddTemplateDialog();
+                break;
+        }
+    }
+
+    private void showAddTemplateDialog() {
+        final Dialog dialog = new Dialog(getActivity());
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        dialog.setCancelable(false);
+        dialog.setContentView(R.layout.add_template_lyt);
+
+        EditText noOfSections = (EditText) dialog.findViewById(R.id.no_of_sections);
+        Button dialogButton = (Button) dialog.findViewById(R.id.proceed_btn);
+        dialogButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getActivity(), )
+                dialog.dismiss();
+            }
+        });
+
+        dialog.show();
+    }
 }
